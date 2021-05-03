0be66908d71eb13b2b5026020f1232a90f846ab4
Betty Pan <betty19931218@gmail.com>
2021-05-03 10:37:11 +0800

    Initial Commit

---
+++ spendingTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spendingTracker
+//
+//  Created by Betty Pan on 2021/5/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
